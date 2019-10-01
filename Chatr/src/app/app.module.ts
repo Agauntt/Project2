f2563ffa9f0db9a320a774202b7bd6cc137b27d2
Aaron <gauntta@gmail.com>
2019-10-01 00:38:40 -0500

    setting up for merge to test/master

--- Chatr/src/app/app.module.ts
+++ Chatr/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { ChatWindowComponent } from './chat-window/chat-window.component';
 
 @NgModule({
